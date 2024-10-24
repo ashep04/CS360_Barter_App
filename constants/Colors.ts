ef23c44594f96ddc20f22a04aa2ad69c4966ed48
Aiden Shepard <shep8121@vandals.uidaho.edu>
2024-10-24 11:15:20 -0700

    Updated colors and Long Logo

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,21 +3,21 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#577399';
+const tintColorDark = '#DBE4EE';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#2F242C',
+    background: '#DBE4EE',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#DBE4EE',
+    background: '#2F242C',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
